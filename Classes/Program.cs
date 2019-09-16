06ce11ffaa6f7fd9a92bf354da67fae38bb9970b
Joaquim Barreto <joaquim@barreto.co>
2019-09-16 18:03:45 +0200

    Done

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,7 +10,11 @@ namespace Classes
     {
         static void doWork()
         {
-            // TODO:
+            Point origin = new Point();
+            Point bottomRight = new Point(1366, 768);
+            double distance = origin.DistanceTo(bottomRight);
+            Console.WriteLine($"Distance is: {distance}");
+            Console.WriteLine($"Number of Point Objects: {Point.ObjectCount()}");
         }
 
         static void Main(string[] args)
